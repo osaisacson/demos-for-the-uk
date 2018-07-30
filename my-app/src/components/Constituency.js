3b93638de8e016d109ffac0d356a65b3e91e3375
BenjaKemp <BenjoKemp@gmail.com>
2018-07-30 12:52:15 +0200

    postcode validation

--- my-app/src/components/Constituency.js
+++ my-app/src/components/Constituency.js
@@ -5,45 +5,37 @@ export class Constituency extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      mp: []
+      postcode: ''
     };
 
-this.consitituencyList = this.consitituencyList.bind(this)
-this.list = this.list.bind(this)
+this.validatePC = this.validatePC.bind(this)
+this.handleChange = this.handleChange.bind(this)
   }
-componentDidMount() {
-  this.consitituencyList();
+
+validatePC(e){
+e.preventDefault()
+let pc = this.state.postcode.toUpperCase().replace(/\s+/g, "");
+pc = pc.replace(/\-/g, "");
+  const valid = /[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}/i;
+  const ok = valid.test(pc);
+  if (!ok){
+    return "invalid";
+  }
+  else{
+    return pc;
+  }
 }
-  consitituencyList() {
-    fetch("http://lda.data.parliament.uk/members.json?_view=members&_pageSize=5&_page=0")
-      .then(res => res.json())
-      .then(res=> {
-        let mpData = res.result.items;
-        mpData.forEach(el => {
-          let newMp = {
-            fullname: el.fullName,
-            homepage: el.homePage,
-            twitter: el.twitter,
-          };
-          console.log(newMp);
-          this.setState({ mp: this.state.mp.concat(newMp)})
-        })
-      })
-      console.log(this.state.mp)
-  }
-  list(e){
-    e.preventDefault();
-    console.log(this.state.mp)
-  }
+handleChange(e) {
+    this.setState({[e.target.name]: e.target.value})
+}
 
   render() {
     return (
       <div id="layout-content" className="layout-content-wrapper">
       <h2>enter your consituency</h2>
-        <form onSubmit={this.consitituencyList}>
-        <button onClick={this.list}>click me</button>
-          <input type="text"
-          name="constituency"/>
+        <form onSubmit={this.validatePC}>
+          <input type="text" onChange={this.handleChange}
+          name="postcode"/>
         </form>
       </div>
     );
---
+++ my-app/src/helperFunctions/ValidatePostCode.js
@@ -0,0 +1,10 @@
+/*We have to validate that the postal code is all upperCase,
+that it has only 6 characters without white space nor dash
+and that these characters follow LNL NLN and have the valide letters
+*/
+
+
+
+module.exports = {
+  validatePC: validatePC
+};
